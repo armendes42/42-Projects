09a3c0d2ef85f31c1b6ca5dd20c1976a1080c3db
Arthur MENDES <armendes@e2r11p15.clusters.42paris.fr>
2022-01-05 20:00:01 +0100

    push_swap tri du debut

---
+++ 2-Cercle/push_swap/includes/push_swap.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: armendes <armendes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/05 17:29:27 by armendes          #+#    #+#             */
+/*   Updated: 2022/01/05 19:58:48 by armendes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+void	bubble_sort(int *tab, int size);
+
+#endif
