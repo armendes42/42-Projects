5866aca0b9c8fa0d12ec965ca7f8b021a10b6603
Arthur MENDES <armendes@e2r13p12.clusters.42paris.fr>
2022-01-19 18:30:00 +0100

    debut debut de philo

---
+++ 3-Cercle/philosophers/philo/includes/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: armendes <armendes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/19 18:22:18 by armendes          #+#    #+#             */
+/*   Updated: 2022/01/19 18:29:38 by armendes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
