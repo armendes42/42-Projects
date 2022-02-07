6590110c8cac5b347823e753c849115b5951f87d
Arthur MENDES <armendes@e2r5p4.clusters.42paris.fr>
2022-02-07 18:00:50 +0100

    minishell un peu mieux

--- 3-Cercle/minishell/includes/minishell.h
+++ 3-Cercle/minishell/includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: armendes <armendes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 15:34:18 by armendes          #+#    #+#             */
-/*   Updated: 2022/02/04 16:39:05 by armendes         ###   ########.fr       */
+/*   Updated: 2022/02/07 17:19:58 by armendes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,6 @@ typedef	struct	s_cmd
 
 t_cmd	*cut_cmd_line(char *line);
 int		check_pipe(t_cmd **cmd_line);
+int		simple_quote(char *cmd);
 
 #endif
--- 3-Cercle/minishell/parsing/parsing.c
+++ 3-Cercle/minishell/parsing/parsing.c
@@ -6,11 +6,42 @@
 /*   By: armendes <armendes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 15:51:03 by armendes          #+#    #+#             */
-/*   Updated: 2022/02/04 17:08:06 by armendes         ###   ########.fr       */
+/*   Updated: 2022/02/07 17:43:07 by armendes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	get_cmd_args(char *cmd)
+{
+	int	i;
+	int	control_simple;
+	int	control_double;
+
+	i = 0;
+	control_simple = 0;
+	control_double = 0;
+	while (cmd[i])
+	{
+		if (cmd[i] == '\'' && (i > 0 && cmd[i - 1] != '\\')
+			&& control_simple == 0)
+		{
+			write(0, &cmd[i], ft_strlen(&cmd[i]));
+			if (simple_quote(&cmd[i]) == 1)
+				control_simple = 1;
+		}
+		if (cmd[i] == '\'' && (i > 0 && cmd[i - 1] != '\\')
+			&& control_simple == 1)
+			control_simple = 0;
+		if (cmd[i] == '"' && control_double == 0)
+		{
+			//if (double_quote(&cmd[i]) == 1)
+				control_double = 1;
+		}
+		i++;
+	}
+	return (0);
+}
 
 static int	parsing(char *line)
 {
@@ -28,6 +59,12 @@ static int	parsing(char *line)
 		return (-1);
 	if (check_pipe(&cmd_line))
 		return (-1);
+	while (cmd_line)
+	{
+		if (get_cmd_args(cmd_line->cmd))
+			return (-1);
+		cmd_line = cmd_line->next;
+	}
 	return (0);
 }
 
