563888086910239c7269cd26608210fa051f2809
Arthur MENDES <armendes@e1r12p15.clusters.42paris.fr>
2022-05-10 16:29:44 +0200

    CPP04 ex01 fini

--- 4-Cercle/CPP/CPP04/ex01/main.cpp
+++ 4-Cercle/CPP/CPP04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: armendes <armendes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 18:15:22 by armendes          #+#    #+#             */
-/*   Updated: 2022/05/10 16:24:25 by armendes         ###   ########.fr       */
+/*   Updated: 2022/05/10 16:29:02 by armendes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,31 +18,6 @@
 
 int main(void)
 {
-  // const Animal* meta = new Animal();
-  // const Animal* j = new Dog();
-  // const Animal* i = new Cat();
-  //
-  // std::cout << j->getType() << " " << std::endl;
-  // std::cout << i->getType() << " " << std::endl;
-  // i->makeSound(); //will output the cat sound!
-  // j->makeSound();
-  // meta->makeSound();
-  //
-  // delete meta;
-  // delete j;
-  // delete i;
-  //
-  // const WrongAnimal* a = new WrongAnimal();
-  // const WrongAnimal* b = new WrongCat();
-  //
-  // std::cout << a->getType() << " " << std::endl;
-  // std::cout << b->getType() << " " << std::endl;
-  // a->makeSound();
-  // b->makeSound();
-  //
-  // delete a;
-  // delete b;
-
   Animal* animalTab[10];
   for (int i = 0; i < 10; i++)
   {
