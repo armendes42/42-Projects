482c8b386dc32b4e53781e7eda9faec15abb4dc0
Arthur MENDES <arthursednem@gmail.com>
2022-06-02 17:09:28 +0200

    CPP08 ex00 fini

--- 4-Cercle/CPP/CPP08/ex00/easyfind.hpp
+++ 4-Cercle/CPP/CPP08/ex00/easyfind.hpp
@@ -6,25 +6,14 @@
 /*   By: armendes <armendes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 16:32:49 by armendes          #+#    #+#             */
-/*   Updated: 2022/06/02 16:43:29 by armendes         ###   ########.fr       */
+/*   Updated: 2022/06/02 17:09:15 by armendes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
-# include <iostream>
-# include <string>
-# include <exception>
-
-class NotFoundException : public std::exception
-{
-  public:
-    virtual const char *what() const throw()
-    {
-      return ("Not found");
-    }
-}
+# include <algorithm>
 
 template <typename T>
 typename T::iterator easyfind(T &array, int n)
@@ -32,7 +21,7 @@ typename T::iterator easyfind(T &array, int n)
   typename T::iterator result = find(array.begin(), array.end(), n);
 
   if (result == array.end())
-    throw NotFoundException();
+    return (array.end());
   return (result);
 }
 
