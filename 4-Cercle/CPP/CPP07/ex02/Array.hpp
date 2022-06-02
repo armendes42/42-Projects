5ce2b5133a485fe9b0f54b7125b58f3cef9a7384
Arthur MENDES <arthursednem@gmail.com>
2022-06-02 16:11:11 +0200

    CPP07 fini

---
+++ 4-Cercle/CPP/CPP07/ex02/Array.hpp
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: armendes <armendes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/02 15:26:45 by armendes          #+#    #+#             */
+/*   Updated: 2022/06/02 16:10:50 by armendes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+# include <string>
+
+template <typename T>
+class Array{
+
+private:
+  T * _array;
+  unsigned int _size;
+
+public:
+	Array(void);
+  Array(unsigned int n);
+	Array(Array const &A);
+	~Array(void);
+	Array &operator=(Array const &A);
+  T &operator[](unsigned int const n) const;
+
+  unsigned int size(void) const;
+
+  class OutOfRangeException : public std::exception{
+
+  public:
+    virtual const char *what() const throw()
+    {
+      return ("Out of Range");
+    }
+
+  };
+
+};
+
+template <typename T>
+Array<T>::Array(void) : _array(NULL), _size(0)
+{
+  return;
+}
+
+template <typename T>
+Array<T>::Array(unsigned int n) : _array(new T[n]), _size(n)
+{
+  for (unsigned int i = 0; i < n; i++)
+    this->_array[i] = T();
+  return;
+}
+
+template <typename T>
+Array<T>::Array(Array const &A) : _array(NULL), _size(0)
+{
+  *this = A;
+  return;
+}
+
+template <typename T>
+Array<T> &Array<T>::operator=(Array const &A)
+{
+  if (this == &A)
+    return (*this);
+  else
+  {
+    if (this->_size != 0 && this->_array != NULL && this->_size != A._size)
+      delete[] this->_array;
+    this->_size = A._size;
+    this->_array = new T[this->_size];
+    for (unsigned int i = 0; i < this->_size; i++)
+      this->_array[i] = A._array[i];
+  }
+  return (*this);
+}
+
+template <typename T>
+Array<T>::~Array(void)
+{
+  if (this->_array != NULL)
+    delete[] this->_array;
+}
+
+template <typename T>
+T &Array<T>::operator[](unsigned int n) const
+{
+  if (n >= this->_size)
+    throw OutOfRangeException();
+  return (this->_array[n]);
+}
+
+template <typename T>
+unsigned int Array<T>::size(void) const
+{
+  return (this->_size);
+}
+
+#endif
---
+++ 4-Cercle/CPP/CPP07/ex02/main.cpp
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: armendes <armendes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/02 15:26:41 by armendes          #+#    #+#             */
+/*   Updated: 2022/06/02 16:10:46 by armendes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstdlib>
+#include "Array.hpp"
+
+#define MAX_VAL 750
+int main(int, char**)
+{
+    std::cout << "* \033[1;37m[INT]\033[0m -------------------- *" << std::endl;
+    {
+      Array<int>array(5);
+      Array<int>copy(array);
+
+      try {
+        for (unsigned int i = 0; i < array.size(); i++)
+          array[i] = i;
+
+        for (unsigned int i = 0; i < copy.size(); i++)
+          copy[i] = i + 10;
+
+        for (unsigned int i = 0; i < array.size(); i++)
+          std::cout << array[i] << " ";
+
+        std::cout << std::endl;
+
+        for (unsigned int i = 0; i < copy.size(); i++)
+          std::cout << copy[i] << " ";
+
+        std::cout << std::endl;
+
+        std::cout << array[6] << std::endl;
+      }
+      catch (std::exception & e)
+      {
+        std::cout << e.what() << std::endl;
+      }
+      std::cout << std::endl;
+    }
+
+    std::cout << "* \033[1;37m[FLOAT]\033[0m ------------------ *" << std::endl;
+    {
+      Array<float>array(3);
+      Array<float>copy;
+
+      float	floats[3] = {9.999, 99.9, 999.99};
+      unsigned int sz = array.size();
+      try {
+        for (unsigned int i = 0; i < sz; i++)
+          array[i] = floats[i];
+
+        copy = array;
+
+        for (unsigned int i = 0; i < sz; i++)
+          std::cout << array[i] << " ";
+        std::cout << std::endl;
+        for (unsigned int i = 0; i < copy.size(); i++)
+          std::cout << copy[i] << " ";
+        std::cout << std::endl;
+
+        std::cout << std::endl;
+
+        for (unsigned int i = 0; i < sz; i++)
+          array[i] = array[i] + 1;
+
+        for (unsigned int i = 0; i < sz; i++)
+          std::cout << array[i] << " ";
+        std::cout << std::endl;
+
+        for (unsigned int i = 0; i < copy.size(); i++)
+          std::cout << copy[i] << " ";
+        std::cout << std::endl;
+
+        std::cout << array[10] << std::endl;
+      }
+      catch (std::exception & e) {
+        std::cout << e.what() << std::endl;
+      }
+      std::cout << std::endl;
+    }
+
+    std::cout << "* \033[1;37m[STRINGS]\033[0m ---------------- *" << std::endl;
+    {
+      Array<std::string>array(4);
+
+      try {
+        array[0] = "Ce n'est pas";
+        array[1] = "un sprint";
+        array[2] = "mais";
+        array[3] = "un marathon";
+
+        for (unsigned int i = 0; i < array.size(); i++) {
+          std::cout << array[i] << " ";
+        }
+        std::cout << std::endl;
+
+        std::cout << array[12] << std::endl;
+      }
+      catch (std::exception & e) {
+        std::cout << e.what() << std::endl;
+      }
+    }
+
+    return (0);
+}
