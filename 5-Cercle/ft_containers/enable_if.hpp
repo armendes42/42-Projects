8e353a5e53063028e68b40f275f34136b3847af5
Arthur MENDES <arthursednem@gmail.com>
2022-09-08 17:16:52 +0200

    enable if fini

--- 5-Cercle/ft_containers/enable_if.hpp
+++ 5-Cercle/ft_containers/enable_if.hpp
@@ -6,13 +6,20 @@
 /*   By: armendes <armendes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 15:59:58 by armendes          #+#    #+#             */
-/*   Updated: 2022/09/08 16:07:48 by armendes         ###   ########.fr       */
+/*   Updated: 2022/09/08 17:16:42 by armendes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENABLE_IF_HPP
 # define ENABLE_IF_HPP
 
+template<bool Cond, class T = void>
+struct enable_if {};
 
+template<class T>
+struct enable_if<true, T>
+{
+	typedef T type;
+};
 
 #endif
