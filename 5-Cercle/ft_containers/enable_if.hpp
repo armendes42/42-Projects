5f29b19c6712f081b5ebda7546bf69b7a954899e
Arthur MENDES <arthursednem@gmail.com>
2022-09-08 19:04:56 +0200

    equal and lexicographical

--- 5-Cercle/ft_containers/enable_if.hpp
+++ 5-Cercle/ft_containers/enable_if.hpp
@@ -6,7 +6,7 @@
 /*   By: armendes <armendes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 15:59:58 by armendes          #+#    #+#             */
-/*   Updated: 2022/09/08 17:40:46 by armendes         ###   ########.fr       */
+/*   Updated: 2022/09/08 19:02:08 by armendes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 namespace ft
 {
 
-	template<bool Cond, class T = void>
+	template <bool Cond, class T = void>
 	struct enable_if {};
 
 	template<class T>
---
+++ 5-Cercle/ft_containers/lexicographical_compare.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexicographical_compare.hpp                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: armendes <armendes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/08 19:02:36 by armendes          #+#    #+#             */
+/*   Updated: 2022/09/08 19:04:41 by armendes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LEXICOGRAPHICAL_COMPARE_HPP
+# define LEXICOGRAPHICAL_COMPARE_HPP
+
+namespace ft
+{
+
+	template <class InputIt1, class InputIt2>
+	bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2)
+	{
+		for ( ; (first1 != last1) && (first2 != last2); ++first1, (void) ++first2 )
+		{
+        if (*first1 < *first2) return true;
+        if (*first2 < *first1) return false;
+    }
+    return (first1 == last1) && (first2 != last2);
+	}
+
+}
+
+#endif
