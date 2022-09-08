8f765ac6a76641e55e91499c7580e8933b0d8539
Arthur MENDES <arthursednem@gmail.com>
2022-09-08 19:54:51 +0200

    debut de vector

--- 5-Cercle/ft_containers/vector.hpp
+++ 5-Cercle/ft_containers/vector.hpp
@@ -1,11 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: armendes <armendes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/08 15:04:44 by armendes          #+#    #+#             */
-/*   Updated: 2022/09/08 15:05:57 by armendes         ###   ########.fr       */
+/*   Created: 2022/09/08 19:37:19 by armendes          #+#    #+#             */
+/*   Updated: 2022/09/08 19:54:43 by armendes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+namespace ft
+{
+
+	template <typename T>
+	class vector
+	{
+		private:
+
+	};
+	
+}
+
+#endif
