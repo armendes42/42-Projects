ec694c45f3e6eba0591c85dd213d91fcefc5fc74
Arthur MENDES <arthursednem@gmail.com>
2022-12-05 19:54:35 +0100

    create table and finish

--- 5-Cercle/inception/srcs/requirements/mariadb/tools/mariadb.sh
+++ 5-Cercle/inception/srcs/requirements/mariadb/tools/mariadb.sh
@@ -4,13 +4,17 @@ mysql_install_db
 
 /etc/init.d/mysql start
 
-if [ -d "/var/lib/mysql/$MYSQL_DATABASE"]
+#Check if the database exists
+
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
 then
-    echo "Database already exists"
+
+	echo "Database already exists"
 else
 
-mysql_secure_installation << _EOF_
+# Set root option so that connexion without root password is not possible
 
+mysql_secure_installation <<_EOF_
 Y
 root123!
 root123!
@@ -20,10 +24,14 @@ Y
 Y
 _EOF_
 
-echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+#Add a root user on 127.0.0.1 to allow remote connexion
 
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRNAT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+	echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
 
+#Create database and user for wordpress
+	echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+
+#Import database
 mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
 
 fi
