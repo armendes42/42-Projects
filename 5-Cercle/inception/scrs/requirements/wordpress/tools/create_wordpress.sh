e6e697548d22d169252c14f87460397dca81b361
Arthur MENDES <arthursednem@gmail.com>
2022-12-12 18:53:41 +0100

    inception fini normalement

--- 5-Cercle/inception/scrs/requirements/wordpress/tools/create_wordpress.sh
+++ 5-Cercle/inception/scrs/requirements/wordpress/tools/create_wordpress.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
-if [ -f ./wordpress/wp-config.php ]
+#check if wp-config.php exist
+if [ -f ./wp-config.php ]
 then
 	echo "wordpress already downloaded"
 else
-	#Download wordpress
-	wget https://wordpress.org/latest.tar.gz
-	tar -xzvf latest.tar.gz
-	rm -rf latest.tar.gz
 
-	#Update configuration file
-	rm -rf /etc/php/7.3/fpm/pool.d/www.conf
-	mv ./www.conf /etc/php/7.3/fpm/pool.d/
+	#Download wordpress and all config file
+	wget http://wordpress.org/latest.tar.gz
+	tar xfz latest.tar.gz
+	mv wordpress/* .
+	rm -rf latest.tar.gz
+	rm -rf wordpress
 
 	#Inport env variables in the config file
-	cd /var/www/html/wordpress
 	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
 	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
 	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
 	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	mv wp-config-sample.php wp-config.php
+	cp wp-config-sample.php wp-config.php
+
 fi
 
 exec "$@"
